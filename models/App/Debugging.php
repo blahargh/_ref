357e2f1790fcd26731700f8d69f47b33eaf7b764
Santos <santos.134@rf.ohio-state.edu>
2018-01-26 11:18:44 -0500

    Added \App\Debugging::formatException() for easy error reporting.

--- models/App/Debugging.php
+++ models/App/Debugging.php
@@ -34,6 +34,29 @@ class Debugging
 		return $propel_debug->getLastExecutedQuery();
 	}
 
+    /**
+     * Format an Exception message that shows the passed in message, the exception message, the line, line number,
+     * and some trace. This can then be passed on to whatever error handling the developer wants.
+     */
+    public static function formatException($message, $exception)
+    {
+        $separator = "<br >\n\t\t\t";
+        $trace = $exception->getTraceAsString();
+        $trace = preg_split('/(?:^|\s)#[0-9]+\s/', $trace); // The very first substring ("^#0 ") is also included as a delimiter so it is removed along with the others. As a consequence, the resulting array starts with an empty element.
+        $filtered = array();
+        $count = 0;
+        foreach ($trace as $tr) {
+            if ($tr == '') { continue; }
+            $count += 1;
+            foreach (self::$traceIgnores as $ignore) {
+                if (strpos($tr, $ignore) !== false) { continue 2; }
+                if (strpos($tr, str_replace('/', '\\', $ignore)) !== false) { continue 2; }
+            }
+            $filtered[] = "#$count $tr";
+        }
+        return "$message{$separator}{$exception->getMessage()}{$separator}In file: {$exception->getFile()}{$separator}Line: {$exception->getLine()}{$separator}Trace:{$separator}".implode($separator, $filtered)."{$separator}";
+    }
+    
 	public static function log($debugMessage = null)
 	{
         if ($debugMessage) {
